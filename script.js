2a9b32bb97c62058da5f0e5b11cd61e66488d737
snowiee04 <khansummayh@gmail.com>
2024-08-01 01:04:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,3 @@ document.getElementById('yes-btn').addEventListener('click', function() {
     container.innerHTML = `<h1>I knew we were meant to be soulmates🥹</h1>`;
 });
 
-document.getElementById('yes-btn').addEventListener('click', function() {
-    container.innerHTML = `<h1>I knew we were meant to be soulmates🥹</h1>`;
-});
-
